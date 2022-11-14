edbe4647772838fadff5f3ea97d5aa7573487e6b
KBluePurple <dbdnjstjr613@gmail.com>
2022-11-15 02:11:30 +0900

    SSR 템플릿

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Counter from './Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Main</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+</section>
+
+<style>
+
+</style>
